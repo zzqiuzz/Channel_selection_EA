108bb9298723cc9af2eb5e016df16dfcfa8a5140
Zhe Zheng <zzqiuzz@gmail.com>
2018-08-02 18:56:44 +0800

    Update select.m

--- select.m
+++ select.m
@@ -2,19 +2,19 @@ function [selected_pop]=select(pop,popsize,chromosize,sum_error,errortable)
 
 selected_pop(popsize,chromosize)=0;
 for i=1:popsize
-    r = rand *sum_error;%随机生成一个随机数，在0和总适应度之间，因为fitness_table(pop_size)为总适应度
+    r = rand *sum_error;
     first = 1;
     last = popsize;
-    mid = round((last+first)/2);%计算总适应度中值
+    mid = round((last+first)/2);
     idx = -1;
-    while (first <= last) && (idx == -1) %按照排中法选择个体
-        if r > errortable(mid)%如果介于中值和总适应度之间
-            first = mid;%第一个值取中值继续循环
-        elseif r < errortable(mid)%同理
+    while (first <= last) && (idx == -1) 
+        if r > errortable(mid)
+            first = mid;
+        elseif r < errortable(mid)
             last = mid;
         else
             idx = mid;
-            break;%一直循环直到得到中值时跳出循环
+            break;
         end
         mid = round((last+first)/2);
         if (last - first) == 1
@@ -23,7 +23,7 @@ for i=1:popsize
         end
     end
     for j=1:chromosize
-        selected_pop(i,j)=pop(idx,j);%遗传到下一代种群
+        selected_pop(i,j)=pop(idx,j);
     end
 end
 
