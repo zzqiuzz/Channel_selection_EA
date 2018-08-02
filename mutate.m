2fe0195b270aa869d2c93d89e9fa6b7e0f75c459
Zhe Zheng <zzqiuzz@gmail.com>
2018-08-02 18:54:59 +0800

    Update mutate.m

--- mutate.m
+++ mutate.m
@@ -1,10 +1,10 @@
-function [pop]=mutate(pop,popsize, chromosize, mutaterate)%变异操作函数
+function [pop]=mutate(pop,popsize, chromosize, mutaterate)
  for i=1:popsize
-    if rand < mutaterate%生成一个随机数判断是否需要变异（mutate_rate为变异概率，一般为一个比较小的实数）
-        mutate_pos1 = ceil(rand*chromosize);%确定变异位置
+    if rand < mutaterate
+        mutate_pos1 = ceil(rand*chromosize);
         mutate_pos2=ceil(rand*chromosize);
-        pop(i,mutate_pos1) = 1 - pop(i, mutate_pos1);%对该位置的二进制数字进行变异：1变成0, 0变成1.
-        pop(i,mutate_pos2) = 1 - pop(i, mutate_pos2);%对该位置的二进制数字进行变异：1变成0, 0变成1.
+        pop(i,mutate_pos1) = 1 - pop(i, mutate_pos1);
+        pop(i,mutate_pos2) = 1 - pop(i, mutate_pos2);
     end
  end 
 end
